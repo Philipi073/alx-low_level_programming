0179e787ebe8f68295a2d53d542f0a9c6a889ab9
Okoye Philip <okoyephlp07322@gmail.com>
2022-09-12 01:37:48 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: combination of numbers
+ * Return:0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar('0' + digit);
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

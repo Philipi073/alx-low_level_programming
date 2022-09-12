f743f25f6b765ea7635e54cbc2ffef244b6edad8
Okoye Philip <okoyephlp07322@gmail.com>
2022-09-12 01:57:51 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: outputs numbers
+ * Return:0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}

162d55941582bf06918a0634dd6c86944e53ebb4
Okoye Philip <okoyephlp07322@gmail.com>
2022-09-14 10:16:34 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of a number
+ * @c: integer
+ *
+ * Return: Absolute value
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (0 - c);
+}

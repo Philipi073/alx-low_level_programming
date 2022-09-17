3ab3b1dca801b015f1295a4dd1ccf56592016fa5
Okoye Philip <okoyephlp07322@gmail.com>
2022-09-17 04:53:10 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10); /* 10 is the ASCII for new line */
+}

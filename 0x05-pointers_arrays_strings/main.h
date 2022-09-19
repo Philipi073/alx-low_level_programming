41be96b7547512b076ae724368681ec4bc95d008
Okoye Philip <okoyephlp07322@gmail.com>
2022-09-19 03:59:14 -0700

    hot fix

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+
+void puts2(char *str);
+
+#endif

fe6499e9e221bd629db8293bca79d46f825eb3e3
Okoye Philip <okoyephlp07322@gmail.com>
2022-10-03 03:48:06 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - multiplies two numbers followed by newline or
+ * print Error if prigram does not recieve teo arguments
+ * @argc: argument count
+ * @argv: array of pointers
+ *
+ * Return: 0(success) or 1 if not two arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}

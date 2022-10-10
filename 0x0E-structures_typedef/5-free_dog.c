209dd10d903b6e4b8f90f5e16f4191d7b020d3b0
Okoye Philip <okoyephlp07322@gmail.com>
2022-10-10 10:39:49 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+nclude "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: pointer to the dogs
+ *
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

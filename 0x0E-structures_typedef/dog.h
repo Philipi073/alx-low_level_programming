b5fa2eceb58df24b8df209508de5eefb563d7e8b
Okoye Philip <okoyephlp07322@gmail.com>
2022-10-10 09:17:03 -0700

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - Defines a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog with the following elements
+ * name, type = char *::age, type = float::owner, type = char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
